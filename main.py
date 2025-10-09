72bcc1be24d9710e1d3f0b44d0994d96eea5cb7a
AJ <avrieland@gmail.com>
2025-10-09 07:32:04 -0400

    Initial Commit

---
+++ main.py
@@ -0,0 +1,77 @@
+import dotenv
+import discord
+from discord.ext import commands
+from discord import app_commands, SelectMenu
+import sqlbot
+
+import random
+
+
+MY_GUILD = discord.Object(id=957056959901732934)
+
+class MyClient(discord.Client):
+    # Suppress error on the User attribute being None since it fills up later
+    user: discord.ClientUser
+
+    def __init__(self, *, intents: discord.Intents):
+        super().__init__(intents=intents)
+        # A CommandTree is a special type that holds all the application command
+        # state required to make it work. This is a separate class because it
+        # allows all the extra state to be opt-in.
+        # Whenever you want to work with application commands, your tree is used
+        # to store and work with them.
+        # Note: When using commands.Bot instead of discord.Client, the bot will
+        # maintain its own tree instead.
+        self.tree = app_commands.CommandTree(self)
+
+    # In this basic example, we just synchronize the app commands to one guild.
+    # Instead of specifying a guild to every command, we copy over our global commands instead.
+    # By doing so, we don't have to wait up to an hour until they are shown to the end-user.
+    async def setup_hook(self):
+        # This copies the global commands over to your guild.
+        self.tree.copy_global_to(guild=MY_GUILD)
+        await self.tree.sync(guild=MY_GUILD)
+
+sql_connector = sqlbot.sqlBot()
+
+intents = discord.Intents.default()
+intents.message_content = True
+client = MyClient(intents=intents)
+
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+    #await bot.sync()
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if "sword" in message.content.lower():
+        await message.channel.send(':fencer:')
+
+@client.tree.command(name="quote", description="returns a random quote")
+async def quote(interaction: discord.Interaction):
+    #await interaction.response.send_message(quote)
+    quote = sql_connector.getRandomQuote()
+    await interaction.response.send_message("Quote #{}: '{}' - {}".format(quote[0], quote[1], quote[3]))
+
+@client.tree.command(name="add_quote", description="Add quote to list")
+async def add_quote(interaction: discord.Interaction, quote: str):
+    user = interaction.user.name
+    quote_return = sql_connector.addQuote(quote, user)
+    await interaction.response.send_message("Quote #{}: '{}' - {}".format(quote_return[0], quote_return[1], quote_return[3]))
+
+@client.tree.command(name="add_tick", description="Add tick to list")
+async def add_tick(interaction: discord.Interaction, tick: str, user: discord.User):
+    if user is None:
+        response = sql_connector.addTick(tick)
+        await interaction.response.send_message(f"Tick #{response[0]}: {response[1]} for everyone", ephemeral=True)
+    else:
+        response = sql_connector.addUserTick(tick, str(user.name))
+        await interaction.response.send_message(f"Tick #{response[0]}: {response[1]} for {response[2]}", ephemeral=True)
+
+
+client.run(dotenv.dotenv_values("discord.env")['DISCORD_TOKEN'])
---
+++ sqlbot.py
@@ -0,0 +1,56 @@
+import mysql.connector
+import dotenv
+import random
+
+class sqlBot():
+    def __init__(self):
+        keys = dotenv.dotenv_values("db.env")
+        self.mydb = mysql.connector.connect(
+            host=keys["HOST"],
+            user=keys["USER"],
+            password=keys["PASSWORD"],
+            database=keys["DATABASE"]
+        )
+        self.my_cursor = self.mydb.cursor(buffered=True)
+        self.debug = False
+        try:
+            self.my_cursor.execute("SELECT * FROM quote_test")
+        except mysql.connector.Error as error:
+            print(error)
+
+    def getRandomQuote(self):
+        self.my_cursor.execute("SELECT count(*) FROM quote_test")
+        quote_count = self.my_cursor.fetchone()
+        random_quote_id = random.randint(1, quote_count[0])
+        if self.debug:
+            print(quote_count)
+            print(random_quote_id)
+        self.my_cursor.execute(f"SELECT * FROM quote_test WHERE quote_id = {random_quote_id}")
+        quote = self.my_cursor.fetchone()
+        return quote
+
+    def addQuote(self, quote, user):
+        sql = "INSERT INTO quote_test (quote_text, quoted_dttm, user) VALUES (%s, NOW(), %s)"
+        values = (quote, user)
+        self.my_cursor.execute(sql, values)
+        self.mydb.commit()
+        self.my_cursor.execute("SELECT * FROM quote_test order by quote_id desc")
+        return self.my_cursor.fetchone()
+
+    def addUserTick(self, tick, user):
+        sql = "INSERT INTO ticks (keyword, user) VALUES (%s, %s)"
+        values = (tick, user)
+        self.my_cursor.execute(sql, values)
+        self.mydb.commit()
+        self.my_cursor.execute("SELECT * FROM ticks order by id desc")
+        return self.my_cursor.fetchone()
+
+    def addTick(self, tick: str):
+        sql = "INSERT INTO ticks (keyword) VALUES (%s)"
+        self.my_cursor.execute(sql, [tick])
+        self.mydb.commit()
+        self.my_cursor.execute("SELECT * FROM ticks order by id desc")
+        return self.my_cursor.fetchone()
+
+    def getTick(self, tick):
+        pass
